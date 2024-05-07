da93e7bc8563ad3eef42fde97f5825d2894bd43e
biri <62590994+zxcv-12345@users.noreply.github.com>
2024-05-08 00:57:45 +0800

    Update toolbox.sh
    优化：
    *运维工具
      **防火墙放行Cloudfare CDN IP 选项可在ufw上自定义指定端口号进行放行

--- toolbox.sh
+++ toolbox.sh
@@ -169,40 +169,42 @@ while true; do
                 6)
                     # 子菜单，用于防火墙放行Cloudfare CDN IP
                     Cloudfare_CDN_IP_menu="
-                    1. 放行Cloudfare CDN IPv4
-                    2. 放行Cloudfare CDN IPv6
+                    1. IPv4 - 放行Cloudfare CDN IPv4
+                    2. IPv6 - 放行Cloudfare CDN IPv6
                     0. 返回上级菜单
                     "
                     echo "$Cloudfare_CDN_IP_menu"
-                    read -p "请输入子菜单选项数字: " Cloudfare_CDN_IP_choice
+                    read -p "请输入IP协议对应的编号: " Cloudfare_CDN_IP_choice
                     case $Cloudfare_CDN_IP_choice in
                         1)
                             echo "IPv4"
-                            sudo ufw allow from 173.245.48.0/20 to any port [端口] proto tcp
-                            sudo ufw allow from 103.21.244.0/22 to any port [端口] proto tcp
-                            sudo ufw allow from 103.22.200.0/22 to any port [端口] proto tcp
-                            sudo ufw allow from 103.31.4.0/22 to any port [端口] proto tcp
-                            sudo ufw allow from 141.101.64.0/18 to any port [端口] proto tcp
-                            sudo ufw allow from 108.162.192.0/18 to any port [端口] proto tcp
-                            sudo ufw allow from 190.93.240.0/20 to any port [端口] proto tcp
-                            sudo ufw allow from 188.114.96.0/20 to any port [端口] proto tcp
-                            sudo ufw allow from 197.234.240.0/22 to any port [端口] proto tcp
-                            sudo ufw allow from 198.41.128.0/17 to any port [端口] proto tcp
-                            sudo ufw allow from 162.158.0.0/15 to any port [端口] proto tcp
-                            sudo ufw allow from 104.16.0.0/13 to any port [端口] proto tcp
-                            sudo ufw allow from 104.24.0.0/14 to any port [端口] proto tcp
-                            sudo ufw allow from 172.64.0.0/13 to any port [端口] proto tcp
-                            sudo ufw allow from 131.0.72.0/22 to any port [端口] proto tcp
+                            read -p "请输入IPv4端口号: " port
+                            sudo ufw allow from 173.245.48.0/20 to any port $port proto tcp
+                            sudo ufw allow from 103.21.244.0/22 to any port $port proto tcp
+                            sudo ufw allow from 103.22.200.0/22 to any port $port proto tcp
+                            sudo ufw allow from 103.31.4.0/22 to any port $port proto tcp
+                            sudo ufw allow from 141.101.64.0/18 to any port $port proto tcp
+                            sudo ufw allow from 108.162.192.0/18 to any port $port proto tcp
+                            sudo ufw allow from 190.93.240.0/20 to any port $port proto tcp
+                            sudo ufw allow from 188.114.96.0/20 to any port $port proto tcp
+                            sudo ufw allow from 197.234.240.0/22 to any port $port proto tcp
+                            sudo ufw allow from 198.41.128.0/17 to any port $port proto tcp
+                            sudo ufw allow from 162.158.0.0/15 to any port $port proto tcp
+                            sudo ufw allow from 104.16.0.0/13 to any port $port proto tcp
+                            sudo ufw allow from 104.24.0.0/14 to any port $port proto tcp
+                            sudo ufw allow from 172.64.0.0/13 to any port $port proto tcp
+                            sudo ufw allow from 131.0.72.0/22 to any port $port proto tcp
                             ;;
                         2)
                             echo "IPv6"
-                            sudo ufw allow from 2400:cb00::/32 to any port [端口] proto tcp
-                            sudo ufw allow from 2606:4700::/32 to any port [端口] proto tcp
-                            sudo ufw allow from 2803:f800::/32 to any port [端口] proto tcp
-                            sudo ufw allow from 2405:b500::/32 to any port [端口] proto tcp
-                            sudo ufw allow from 2405:8100::/32 to any port [端口] proto tcp
-                            sudo ufw allow from 2a06:98c0::/29 to any port [端口] proto tcp
-                            sudo ufw allow from 2c0f:f248::/32 to any port [端口] proto tcp
+                            read -p "请输入IPv6端口号: " port
+                            sudo ufw allow from 2400:cb00::/32 to any port $port proto tcp
+                            sudo ufw allow from 2606:4700::/32 to any port $port proto tcp
+                            sudo ufw allow from 2803:f800::/32 to any port $port proto tcp
+                            sudo ufw allow from 2405:b500::/32 to any port $port proto tcp
+                            sudo ufw allow from 2405:8100::/32 to any port $port proto tcp
+                            sudo ufw allow from 2a06:98c0::/29 to any port $port proto tcp
+                            sudo ufw allow from 2c0f:f248::/32 to any port $port proto tcp
                             ;;
                         0)
                             echo "返回上级菜单"
